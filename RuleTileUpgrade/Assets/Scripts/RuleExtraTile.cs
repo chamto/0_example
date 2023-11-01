ceca8e364635caf63f6f970ade50419793d232e7
chamto <anrudco@gmail.com>
2023-11-02 02:33:39 +0900

    [ RuleTile 업그레이드 작업 : RuleTile_Custom 추가 , RuleTile_Custom_Editor 추가 ,  border_dir 저장 안되는 문제 수정 ]

--- RuleTileUpgrade/Assets/Scripts/RuleExtraTile.cs
+++ RuleTileUpgrade/Assets/Scripts/RuleExtraTile.cs
@@ -29,22 +29,22 @@ namespace UnityVer2022_03_09_f1
     /// See the cliff tile for an example.
     /// </summary>
     [CreateAssetMenu]
-    public class RuleExtraTile : RuleTile<RuleExtraTile.Neighbor>
+    public class RuleExtraTile : RuleTile_Custom<RuleExtraTile.Neighbor>
     {
         //Type m_NeighborType = typeof<Neighbor>; //타입을 받아온다 
 
-        public class Neighbor : RuleTile.TilingRule.Neighbor
+        public class Neighbor : RuleTile_Custom.TilingRule.Neighbor
         {
             public const int Adjacent = 3; //그리드에 인접타일 지정에 사용  
         }
 
         //TilingRule 의 확장 정보를 가진 객체 , 상속으로 정보를 확장할 수 없어서 추가로 객체를 만듦  
-        public class TilingRule_2
-        {
-            public int _border_dir = 0; //arrows 경계 방향이 들어간다 , eDirection8 방향값으로 변환되어 들어간다 
-        }
+        //public class TilingRule_2
+        //{
+        //    public int _border_dir = 0; //arrows 경계 방향이 들어간다 , eDirection8 방향값으로 변환되어 들어간다 
+        //}
 
-        [HideInInspector] public List<TilingRule_2> m_TilingRules_2 = new List<TilingRule_2>();
+        //[HideInInspector] public List<TilingRule_2> m_TilingRules_2 = new List<TilingRule_2>();
 
         public TileBase[] AdjacentTiles;
 
